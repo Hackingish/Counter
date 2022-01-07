06f175abd305818bd8ac423eb9792158288177a1
Hackingish <subhasispanda876@gmail.com>
2022-01-07 10:00:00 +0530

    Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="container">
+      <div className="counter">
+        <div>
+          <input type="text" placeholder='0'/>
+        </div>
+
+        <div className="button">
+          <button>+</button>
+          <button>-</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
