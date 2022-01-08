b2787112a974d3349d3eaecec59f9d91a91020a3
Hackingish <subhasispanda876@gmail.com>
2022-01-08 10:00:00 +0530

    Counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { useState } from "react";
+
 
 const App = () => {
+
+  const [input,setInput] = useState(0);
+
+  const setIncrement = () =>{
+    setInput((prev) => prev + 1);
+  }
+
+  const setDecrement = () =>{
+    setInput((prev) => prev - 1);
+  }
   return (
     <div className="container">
       <div className="counter">
-        <div>
-          <input type="text" placeholder='0'/>
-        </div>
+        <div className="display">{input || "0"}</div>
 
         <div className="button">
-          <button>+</button>
-          <button>-</button>
+          <button onClick={setIncrement}>+</button>  
+          <button onClick={setDecrement}>-</button>
         </div>
       </div>
     </div>
