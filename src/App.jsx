d83f96e8cc2292d3b2f6ac4efcf556b80510f02b
Hackingish <subhasispanda876@gmail.com>
2022-01-09 10:00:00 +0530

    UI Improvement

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,19 @@ const App = () => {
   const setDecrement = () =>{
     setInput((prev) => prev - 1);
   }
+
+  const setReset = () => {
+    setInput(0)
+  }
   return (
     <div className="container">
       <div className="counter">
         <div className="display">{input || "0"}</div>
 
-        <div className="button">
-          <button onClick={setIncrement}>+</button>  
-          <button onClick={setDecrement}>-</button>
+        <div className="buttons">
+          <button className = "btn" onClick={setIncrement}>+</button>  
+          <button className = "btn" onClick={setDecrement}>-</button>
+          <button className = "btn reset" onClick={setReset}>Reset</button>
         </div>
       </div>
     </div>
